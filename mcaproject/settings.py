66e5ab085cc9b67dcc17f6f44bcf5040394dac85
Susmitha0912 <104626331+Susmitha0912@users.noreply.github.com>
2022-06-18 21:25:43 +0530

    settings.py

--- mcaproject/settings.py
+++ mcaproject/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-cg9$biuf&fqz*1^^8qxg(c^&b7ff9n!h+%d9u9=x2b4y#s(&3p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1","digitala-doption1.herrokuapp.com"]
 
 
 # Application definition
@@ -118,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-import os
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
